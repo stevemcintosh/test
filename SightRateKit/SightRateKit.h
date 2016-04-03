65f3b28fa5e0155f18aeccd95e678cad2e2cfe20
Steve McIntosh <support@ecollage.com.au>
2016-04-03 11:07:01 +0100

    Initial commit

---
+++ SightRateKit/SightRateKit.h
@@ -0,0 +1,19 @@
+//
+//  SightRateKit.h
+//  SightRateKit
+//
+//  Created by Stephen McIntosh on 2/03/2016.
+//  Copyright © 2016 Stephen McIntosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SightRateKit.
+FOUNDATION_EXPORT double SightRateKitVersionNumber;
+
+//! Project version string for SightRateKit.
+FOUNDATION_EXPORT const unsigned char SightRateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SightRateKit/PublicHeader.h>
+
+
--- StaticLogStoreForDevelopment/LogStore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LogStore.swift
-//  test
-//
-//  Created by Stephen McIntosh on 4/03/2016.
-//  Copyright © 2016 Stephen McIntosh. All rights reserved.
-//
-
-import Foundation
